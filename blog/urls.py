ab1241eb11e7982ec9bfb7b79f1635b1a164d79a
Shon23 <shoxxxxx98@gmail.com>
2021-03-19 22:33:45 +0900

    new update

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('posts/', PostListView.as_view(), name='blog-home'),
-    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
--- django_article/settings.py
+++ django_article/settings.py
@@ -124,6 +124,9 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
+STATICFILES_DIR = (
+    os.path.join(BASE_DIR, 'static'),
+                   )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
