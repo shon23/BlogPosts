682064f00d695b6958e1a8008bf800a7cfb7c73d
Shokhrukh Bekmuradov <shon@Shokhrukhs-MacBook-Pro.local>
2021-04-11 16:36:42 +0900

    settings

--- django_article/settings.py
+++ django_article/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "@kgsd$lkfsl^foldfjolsajo;**&DSDLKF"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True#(os.environ.get("DEBUG_VALUE") == "True")
 
-ALLOWED_HOSTS = ['uzbarticles.herokuapp.com', '*']
+ALLOWED_HOSTS = ['blogpostapp1.herokuapp.com', '*']
 
 
 # Application definition
@@ -138,8 +138,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = "shoxxxxx98@gmail.com"
+EMAIL_HOST_PASSWORD = "elmurodaka@"
 django_heroku.settings(locals())
 
 AWS_ACCESS_KEY_ID = "AKIAVHTMTU2EKUZK5N65"
